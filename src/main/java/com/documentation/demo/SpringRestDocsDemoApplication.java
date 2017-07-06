ab79dbb78bf397ee457a3720052798e29e70a05d
fmaffioletti <fmaffioletti@ebay.com>
2017-07-06 16:22:38 +0200

    Init project

---
+++ src/main/java/com/documentation/demo/SpringRestDocsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.documentation.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDocsDemoApplication.class, args);
+	}
+}
